b6e1a154816413c95202793ccb23fbf5fdebb967
Mateusz Skalski <mat.skalski@wp.pl>
2025-09-27 15:45:44 +0200

    zmiana

--- src/app/services/users-with-rxjs.service.spec.ts
+++ src/app/services/users-with-rxjs.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UsersWithRxjsService } from './users-with-rxjs.service';
+import { UserInteface } from '../types/user.interface';
 
 describe('UsersWithRxjsService', () => {
-  let service: UsersWithRxjsService;
+  let usersWithRxjsService: UsersWithRxjsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UsersWithRxjsService);
+    usersWithRxjsService = TestBed.inject(UsersWithRxjsService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(usersWithRxjsService).toBeTruthy();
+  });
+
+  describe('addUser', () => {
+    it('should add a user', () => {
+      const user: UserInteface = {
+        id: '3',
+        name: 'foo',
+      };
+      usersWithRxjsService.addUser(user);
+      expect(usersWithRxjsService.users$.getValue()).toEqual([{ id: '3', name: 'foo' }]);
+    });
+  })
+
+  describe('removeUser', () => {
+    it('should remove a user', () => {
+      usersWithRxjsService.users$.next([{ id: '3', name: 'foo' }]);
+      usersWithRxjsService.removeUser('3');
+      expect(usersWithRxjsService.users$.getValue()).toEqual([]);
+    });
   });
 });
--- src/app/services/users-with-rxjs.service.ts
+++ src/app/services/users-with-rxjs.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { UserInteface } from '../types/user.interface';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersWithRxjsService {
-  
+  users$ = new BehaviorSubject<UserInteface[]>([]);
+
+
+  addUser(user: UserInteface): void {
+    this.users$.next([...this.users$.getValue(), user])
+  }
+
+  removeUser(userId: string): void {
+    const updatedUsers = this.users$
+      .getValue()
+      .filter((user) => userId !== user.id)
+
+    this.users$.next(updatedUsers)
+  }
+
 }
