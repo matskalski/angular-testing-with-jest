07b2070cc7ae51f625ef56449e260e61626abd10
Mateusz Skalski <mat.skalski@wp.pl>
2025-10-03 18:09:47 +0200

    zmiana

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,43 +1,109 @@
 describe('template spec', () => {
   beforeEach(() => {
     //mockowanie requestów do bazy danych
+    //metoda get
     cy.intercept(
       //request
       {
-      method: 'GET',
-      url: 'http://localhost:3004/todos'
-    },
-    //response
-    {
-      body: [
+        method: 'GET',
+        url: 'http://localhost:3004/todos'
+      },
+      //response
+      {
+        body: [
+          {
+            text: 'first todo',
+            isCompleted: true,
+            id: 1,
+          },
+          {
+            text: 'second todo',
+            isCompleted: false,
+            id: 2,
+          },
+          {
+            text: 'third todo',
+            isCompleted: false,
+            id: 3,
+          },
+        ]
+      }
+    )
+      //metoda POST
+      .intercept(//request
         {
-          text: 'first todo',
-          isCompleted: true,
-          id: 1,
+          method: 'POST',
+          url: 'http://localhost:3004/todos'
         },
+        //response
         {
-          text: 'second todo',
-          isCompleted: false,
-          id: 2,
-        },
+          body:
+          {
+            text: 'forth todo',
+            isCompleted: false,
+            id: 4,
+          }
+        })
+      //metoda DELETE
+      .intercept(//request
         {
-          text: 'third todo',
-          isCompleted: false,
-          id: 3,
+          method: 'DELETE',
+          url: 'http://localhost:3004/todos/1'
         },
-      ]
-    }
-  );
+        //response
+        {
+          body:
+          { }
+        })
+      ;
 
-  cy.visit('http://localhost:4200')
+    cy.visit('http://localhost:4200')
   });
 
   it('visit initial page', () => {
     cy.contains('todos')
   });
 
-  it('renders 3 todos', ()=>{
+  it('renders 3 todos', () => {
     cy.get('[data-cy="todo"]').should('have.length', 3);
-    cy.get('[data-cy="todoLabel"]').eq(0).should('contain.text', 'first todo')
+
+    cy.get('[data-cy="todoLabel"]').eq(0).should('contain.text', 'first todo');
+    cy.get('[data-cy="todoLabel"]').eq(1).should('contain.text', 'second todo');
+    cy.get('[data-cy="todoLabel"]').eq(2).should('contain.text', 'third todo');
+
+    cy.get('[data-cy="todoCheckbox"]').eq(0).should('be.checked');
   });
+
+  it('renders footer', () => {
+    cy.get('[data-cy="todoCount"]').should('contain.text', '2 items left');
+    cy.get('[data-cy="filterLink"]').eq(0)
+      .should('contain.text', 'All')
+      .should('have.class', 'selected');
+    cy.get('[data-cy="filterLink"]').eq(1).should('contain.text', 'Active');
+    cy.get('[data-cy="filterLink"]').eq(2).should('contain.text', 'Completed');
+  });
+
+  it('can change filter', () => {
+    //symulacja kliknięcia drugiego elementu na liście
+    cy.get('[data-cy="filterLink"]').eq(1).click()
+    cy.get('[data-cy="filterLink"]').eq(1).should('contain.text', 'Active');
+  })
+
+  it('can add todo', () => {
+    cy.get('[data-cy="newTodoInput"]')
+      //symulacja wypełnienia inputu treścią 'forth todo' i wciśnięcia od razu enter
+      .type('forth todo{enter}');
+
+    cy.get('[data-cy="todoCount"]').should('contain.text', '3 items left');
+    //sprawdzenie czy element został dodany do listy
+    cy.get('[data-cy="todoLabel"]').eq(3).should('contain.text', 'forth todo');
+  })
+
+  it('can remove todo', ()=>{
+    //wymuszenie kliknięcia na elemencie
+    //potrzebne poniewaz element nie jest wyświetlany
+    //a jest potrzeba zasymulowania tego zdarzenia
+    cy.get('[data-cy="destroy"]').eq(0).click({force: true})
+    cy.get('[data-cy="todo"]').should('have.length', 2)
+  })
 })
